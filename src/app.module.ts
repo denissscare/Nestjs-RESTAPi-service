5a2a7ae319928e684f7da180200b91adeb0dc707
denissscare <denissscare@gmail.com>
2023-10-26 20:06:54 +0800

    added new dependencies and update app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { User } from "./users/users.model";
 import { RolesModule } from "./roles/roles.module";
 import { Roles } from "./roles/roles.model";
 import { UserRoles } from "./roles/user-roles.model";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [],
@@ -26,6 +27,7 @@ import { UserRoles } from "./roles/user-roles.model";
     }),
     UsersModule,
     RolesModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
